60b237d6b0760c8492232df2692886e3482b0465
ketanmashalkar <88704853+ketanmashalkar@users.noreply.github.com>
2021-08-19 12:24:27 +0530

    Add files via upload
    "19 aug"

---
+++ Assingment1.py
@@ -0,0 +1,39 @@
+#1)  Reverse a String and print it on console
+#    "Python Skills" .
+x="python skills"
+print("reverse string extract [::-1]",x[::-1])
+
+
+#2)Assign String to x variable in DD-MM-YYYY format
+#  extract and print Year from String.
+x="02082021"
+print("print string extract [4:8] : ",x[4:8])
+
+
+#3)In a small company, the average salary of three employees is Rs1000 per week. If one employee earns Rs1100 and other earns Rs500, how much will the third employee earn?
+s3=3000-1100-500
+
+print("the salary of 3rd employee is ",s3)
+
+#4) Write Program - convert a percentage to a fraction 
+
+num = 30/100
+
+dec_num = num * 100
+  
+
+print ("Decimal equivalent in %")
+  
+
+print (round(dec_num,2))
+
+
+
+
+#5)Write Program - A train 340 m long is running at a speed of 45 km/hr. what time will it take to cross a 160 m long tunnel?
+tunnel=160
+s=45
+speed=45000/3600
+distance=340+160
+time=distance/speed
+print("time required to cross a tunnel is ",time)
